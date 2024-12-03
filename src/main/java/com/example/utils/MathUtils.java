343a0ef13e879f67096cf5baf60d0479c71d81a1
guillermoroman <guillermoroman@outlook.com>
2024-12-03 10:26:59 +0100

    Añadir tests para max

--- src/main/java/com/example/utils/MathUtils.java
+++ src/main/java/com/example/utils/MathUtils.java
@@ -1,4 +1,24 @@
 package com.example.utils;
 
 public class MathUtils {
+
+    public static int max(int x, int y){
+        if (x > y){
+            return x;
+        } else {
+            return y;
+        }
+    }
+
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 4;
+        System.out.println(max(x, y));
+
+        x = 8;
+        y = -10;
+        System.out.println(max(x, y));
+    }
+
+
 }
--- src/test/java/com/example/utils/MathUtilsTest.java
+++ src/test/java/com/example/utils/MathUtilsTest.java
@@ -1,4 +1,18 @@
 package com.example.utils;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MathUtilsTest {
+
+    @Test
+    public void maxWithPositiveNumbers(){
+        int x = 8;
+        int y = 15;
+
+        int esperado = 15;
+        int resultado = MathUtils.max(x, y);
+
+        assertEquals(esperado, resultado);
+    }
 }
